65a89e878cb00fe3e6f1fa22fffa09746c080771
André <anprofelipe@gmail.com>
2020-10-22 16:12:47 -0300

    Integrando ao Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,46 @@ import "package:calculator/screens/calculator.dart";
 import 'package:flutter/services.dart';
 import 'package:firebase_admob/firebase_admob.dart';
 import 'package:admob_flutter/admob_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();  
+  WidgetsFlutterBinding.ensureInitialized();
   Admob.initialize();
   FirebaseAdMob.instance.initialize(appId: 'ca-app-pub-2837828701670824~6493333574');
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {  
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Ocorreu um erro!'),
+          );
+        }
 
-    return MaterialApp(
-      title: 'Calculadora de iPhone',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Calculator(),
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Calculadora de iPhone',
+            theme: ThemeData(
+              primarySwatch: Colors.orange,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            debugShowCheckedModeBanner: false,
+            home: Calculator(),
+          );
+        }
+
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }

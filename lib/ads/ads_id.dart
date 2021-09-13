14fb198792b4d57ac4b9f310287162d531a6eb8f
André Felipe <academicworks.adf@gmail.com>
2021-09-13 18:00:50 -0300

    feat(Ads): add a ad manager and a interstitial ad

---
+++ lib/ads/ads_id.dart
@@ -0,0 +1,5 @@
+class CalculatorAdsID {
+  static const String interstitialId = 'ca-app-pub-2837828701670824/1879398858';
+  static const String rewardedId = 'ca-app-pub-2837828701670824/1879398858';
+  static const String bannerId = 'ca-app-pub-2837828701670824/6086139467';
+}
--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,8 +1,8 @@
+import 'package:calculator/ads/ads_manager.dart';
 import 'package:calculator/components/display.dart';
 import 'package:calculator/components/keyboard.dart';
 import 'package:calculator/models/memory.dart';
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class Calculator extends StatefulWidget {
   @override
@@ -10,14 +10,17 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  final int maxFailedLoadAttempts = 3;
-  int _numRewardedLoadAttempts = 0;
-
-  late AdWidget bannerAdWidget;
-  RewardedAd? _rewardedAd;
-
+  AdsManager adsManager = AdsManager();
   Memory memory = Memory();
-  late BannerAd adBanner;
+  bool isPremium = true;
+
+  @override
+  void initState() {
+    super.initState();
+    if (!isPremium) {
+      loadAds();
+    }
+  }
 
   void _onPressed(String command) {
     setState(() {
@@ -25,75 +28,12 @@ class _CalculatorState extends State<Calculator> {
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    adBanner = BannerAd(
-      adUnitId: 'ca-app-pub-2837828701670824/6086139467',
-      listener: BannerAdListener(),
-      request: AdRequest(),
-      size: AdSize.banner,
-    );
-    adBanner.load();
-    bannerAdWidget = AdWidget(ad: adBanner);
-    _createRewardedAd();
-    tryShowRewardedAd();
-  }
-
-  void _createRewardedAd() {
-    RewardedAd.load(
-      adUnitId: 'ca-app-pub-2837828701670824/1879398858',
-      request: AdRequest(),
-      rewardedAdLoadCallback: RewardedAdLoadCallback(
-        onAdLoaded: (RewardedAd ad) {
-          print('$ad loaded.');
-          _rewardedAd = ad;
-          _numRewardedLoadAttempts = 0;
-        },
-        onAdFailedToLoad: (LoadAdError error) {
-          print('RewardedAd failed to load: $error');
-          _rewardedAd = null;
-          _numRewardedLoadAttempts += 1;
-          if (_numRewardedLoadAttempts <= maxFailedLoadAttempts) {
-            _createRewardedAd();
-          }
-        },
-      ),
-    );
-  }
-
-  void _showRewardedAd() {
-    if (_rewardedAd == null) {
-      print('Warning: attempt to show rewarded before loaded.');
-      tryShowRewardedAd();
-      return;
-    }
-    _rewardedAd!.fullScreenContentCallback = FullScreenContentCallback(
-      onAdShowedFullScreenContent: (RewardedAd ad) =>
-          print('ad onAdShowedFullScreenContent.'),
-      onAdDismissedFullScreenContent: (RewardedAd ad) {
-        print('$ad onAdDismissedFullScreenContent.');
-        ad.dispose();
-        _createRewardedAd();
-      },
-      onAdFailedToShowFullScreenContent: (RewardedAd ad, AdError error) {
-        print('$ad onAdFailedToShowFullScreenContent: $error');
-        ad.dispose();
-        _createRewardedAd();
-      },
-    );
-
-    _rewardedAd!.setImmersiveMode(true);
-    _rewardedAd!.show(onUserEarnedReward: (RewardedAd ad, RewardItem reward) {
-      print('$ad with reward $RewardItem(${reward.amount}, ${reward.type}');
-    });
-    _rewardedAd = null;
-  }
-
-  void tryShowRewardedAd() {
-    Future.delayed(Duration(seconds: 3), () {
-      _showRewardedAd();
-    });
+  void loadAds() {
+    adsManager.createBannerAd();
+    adsManager.createRewardedAd();
+    adsManager.keepTryingShowRewardedAd();
+    adsManager.createInterstitialAd();
+    adsManager.keepTryingShowInterstitialAd();
   }
 
   @override
@@ -104,10 +44,7 @@ class _CalculatorState extends State<Calculator> {
         children: <Widget>[
           SizedBox(height: 35),
           Container(
-            height: adBanner.size.height.toDouble(),
-            width: adBanner.size.width.toDouble(),
-            alignment: Alignment.center,
-            child: bannerAdWidget,
+            child: !isPremium ? adsManager.adBannerWidget() : Container(),
           ),
           Display(memory.value),
           Keyboard(_onPressed),
@@ -118,8 +55,7 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   void dispose() {
-    _rewardedAd?.dispose();
-    adBanner.dispose();
+    adsManager.dispose();
     super.dispose();
   }
 }
